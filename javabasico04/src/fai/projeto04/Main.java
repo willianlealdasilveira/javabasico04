f62017fd52117327e29979da9f047d991351b870
WILLIAN <willianlealdasiveira@outlook.com.br>
2020-03-04 19:41:22 -0300

    primeiro commit

---
+++ javabasico04/src/fai/projeto04/Main.java
@@ -0,0 +1,35 @@
+package fai.projeto04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.numero20valor();
+
+	}
+	
+	private void numero20valor() {
+	 int valor1,valor2,valor3;
+	 
+	 valor1 = 0;
+	 valor2 = 0;
+	 valor3 = 0;
+		
+	 while(valor1 <=19) {
+		 valor1++;
+		 System.out.println("valor1 é " +valor1);
+		 
+	 }
+	 while(valor2 <=19) {
+		 valor2++;
+		 System.out.println("valor2 é " +valor2);
+	 
+	}
+	 
+	 while(valor3 <=19) {
+		 valor3++;
+		 System.out.println("valor3 é " +valor3);
+	 
+	}
+	}
+}
